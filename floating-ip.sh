d1d95f7595f6f62cd5d855e8892141327412021a
Adrian Moisey <adrian@changeover.za.net>
2017-10-30 20:29:39 +0200

    Need some exports

--- floating-ip.sh
+++ floating-ip.sh
@@ -7,13 +7,13 @@ set -eu
 # FLOATING_IP=<the floating ip which you want assigned to this node>
 
 if [ -f /run/secrets/DIGITALOCEAN_ACCESS_TOKEN ]; then
-    DIGITALOCEAN_ACCESS_TOKEN=$(cat /run/secrets/DIGITALOCEAN_ACCESS_TOKEN)
+    export DIGITALOCEAN_ACCESS_TOKEN=$(cat /run/secrets/DIGITALOCEAN_ACCESS_TOKEN)
 else
-    DIGITALOCEAN_ACCESS_TOKEN=${DIGITALOCEAN_ACCESS_TOKEN}
+    export DIGITALOCEAN_ACCESS_TOKEN=${DIGITALOCEAN_ACCESS_TOKEN}
 fi
 
 
-MY_IP=$(curl -s http://169.254.169.254/metadata/v1/id)
+export MY_IP=$(curl -s http://169.254.169.254/metadata/v1/id)
 
 # Only for bash
 #if [[ ! $MY_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
